bedc6da5100f70b483fb8051da13380e09c2bdeb
igor.dopita <igor.dopita@adt.com.au>
2024-07-15 16:40:43 +1000

    Improved the event editor. Still a lot of work to go there.

--- Source/eventseditor.cpp
+++ Source/eventseditor.cpp
@@ -10,7 +10,29 @@ void EventsEditor::ApplyNumStages()
 {
    QTreeWidgetItem* item;
 
-   for (int row = 1; row <= ui_->numStagesSpinBox_->value(); ++row)
+   int numStages = ui_->numStagesSpinBox_->value();
+
+   // Out with the old
+   bool changed;
+   do
+   {
+      changed = false;
+      int numItems = ui_->eventTreeWidget_->topLevelItemCount();
+      for (int row = 0; (changed == false) && (row < numItems); ++row)
+      {
+         QTreeWidgetItem* item = ui_->eventTreeWidget_->topLevelItem(row);
+         if (item->data(0, Qt::DisplayRole).toInt() > numStages)
+         {
+            qDebug() << item->data(0, Qt::DisplayRole).toInt();
+            delete ui_->eventTreeWidget_->takeTopLevelItem(row);
+            changed = true;
+         }
+      }
+   }
+   while (changed == true);
+
+   // And in with the new
+   for (int row = 1; row <= numStages; ++row)
    {
       if (ui_->eventTreeWidget_->findItems(QString::number(row), Qt::MatchExactly, 0).size() == 0)
       {
